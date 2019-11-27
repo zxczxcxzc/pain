ebf569af64f40c6cd6e5576f8e294539a5630938
unknown <51wanderer51@gmail.com>
2019-11-26 23:26:23 -0500

    added text conversion tool

---
+++ asciiconv.py
@@ -0,0 +1,30 @@
+# Utility to convert text to pixels
+# usage: 
+
+import sys
+from PIL import Image
+
+
+if(len(sys.argv) < 2):
+    print("Usage: asciiconv.py [text]")
+    sys.exit()
+
+st = ' '.join(sys.argv[1:])
+
+binstr = []
+for c in st:
+    binstr.append(format(ord(c), 'b')[::-1])
+print(' '.join(binstr))
+
+im = Image.new("RGB", (7, len(binstr) * 2), (255, 255, 255))
+x = 0
+y = 0
+for i in binstr:
+    for c in i:
+        if(c) == "1":
+            im.putpixel((x, y), (0, 0, 0))
+        x += 1
+    x = 0
+    im.putpixel((x, y + 1), (0, 255, 0))
+    y += 2
+im.save("text.png")
